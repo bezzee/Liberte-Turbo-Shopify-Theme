47e108e2adebad274f14abc565abb50874d7bb4f
jakekaminski <jake.k@csm.cc>
2019-06-04 14:18:48 -0500

    Initial All Commit

---
+++ assets/zendesk-embeddable.js
@@ -0,0 +1,1 @@
+var script = document.createElement('script');script.type = 'text/javascript';script.id = 'ze-snippet';script.src = 'https://static.zdassets.com/ekr/snippet.js?key=6766492d-2653-4748-900b-667ec460eef0';document.getElementsByTagName('head')[0].appendChild(script);
